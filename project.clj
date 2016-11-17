6aec7e27c0e1d53d714aecb91a71b7229978f012
jwagner <jwagner@life.uni-leipzig.de>
2016-11-17 11:37:53 +0100

    Move Reference Implementation to dev Dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/plumbing "0.5.2"]
-                 [de.knoppiks.sandbox/iso7064 "0.1-SNAPSHOT"]]
+                 [prismatic/plumbing "0.5.2"]]
 
   :profiles {:dev
              {:source-paths ["dev"]
               :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                             [org.clojure/test.check "0.9.0"]]
+                             [org.clojure/test.check "0.9.0"]
+                             [de.knoppiks.sandbox/iso7064 "0.1-SNAPSHOT"]]
               :global-vars {*print-length* 20}}})
