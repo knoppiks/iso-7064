7217f0294191c6b275012329b98919bcf075a1ab
jwagner <jwagner@life.uni-leipzig.de>
2016-11-17 11:39:11 +0100

    Add Stack Trace Printing Function

--- dev/user.clj
+++ dev/user.clj
@@ -1,2 +1,5 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :refer [refresh]]))
+  (:require [clojure.tools.namespace.repl :refer [refresh]]
+            [clojure.stacktrace :refer [e]]))
+
+(def pst e)
